f51ad458f0e3b948c7c328656edce3800ebfea38
nickeyprod <nickeyprod@gmail.com>
2025-10-30 09:54:02 +0300

    First push

---
+++ em_todo_test/em_todo_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  em_todo_testApp.swift
+//  em_todo_test
+//
+//  Created by Николай Ногин on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct em_todo_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ em_todo_testTests/em_todo_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  em_todo_testTests.swift
+//  em_todo_testTests
+//
+//  Created by Николай Ногин on 30.10.2025.
+//
+
+import Testing
+@testable import em_todo_test
+
+struct em_todo_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
